39e89b90ede215b6896cf7b956928376b0c419a5
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-12-11 08:40:44 +0700

    refactor: use gorm to connection

--- go.mod
+++ go.mod
@@ -22,6 +22,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.13.0 // indirect
 	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
@@ -49,4 +51,6 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.4.5 // indirect
+	gorm.io/gorm v1.24.2 // indirect
 )
--- util/database.go
+++ util/database.go
@@ -1,28 +1,33 @@
 package util
 
 import (
-	"database/sql"
 	"log"
 	"time"
 
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // counts for count time response database
 var counts int64
 
 // Function open connection to Database
-func openDB(dsn string) (*sql.DB, error) {
-	// Open connection
-	db, err := sql.Open("pgx", dsn)
+func openDB(dsn string) (*gorm.DB, error) {
+	// Open connection to db
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	// Ping db
-	err = db.Ping()
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	err = sqlDB.Ping()
 	if err != nil {
 		return nil, err
 	}
@@ -31,7 +36,7 @@ func openDB(dsn string) (*sql.DB, error) {
 }
 
 // Function for connection to db
-func SetupDB(dsn string) *sql.DB {
+func SetupDB(dsn string) *gorm.DB {
 	for {
 		connection, err := openDB(dsn)
 
